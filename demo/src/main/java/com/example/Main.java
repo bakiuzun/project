f5c3ecb228633d16c4f37ba314518764dfc44d86
Dorriere Mehdi <wawaque.astro.game@gmail.com>
2023-11-23 10:24:08 +0100

    ajout et modif menu

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/main.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/menu.fxml"));
         Parent root = loader.load();
 
         Scene scene = new Scene(root, 300, 200);
