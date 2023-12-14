d3a117a0795885fe9d098e2964d95ee12856d3d6
bakiuzun <u.officialdeveloper@gmail.com>
2023-12-14 11:03:38 +0100

    continue party

--- demo/src/main/java/com/example/controller/ContinuePartyController.java
+++ demo/src/main/java/com/example/controller/ContinuePartyController.java
@@ -2,7 +2,10 @@ package com.example.controller;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import javax.security.auth.callback.Callback;
 
 import com.example.model.JsonDatabase;
 import com.example.model.Session;
@@ -11,44 +14,103 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 public class ContinuePartyController implements Initializable {
-    @FXML
-    private AnchorPane anchorPane;
-    @FXML
-    private TableView<Session> tableView;
+
+    public class SessionListViewCell extends ListCell<Session> {
+
+        private SessionHBox sessionHBox;
+        
+        @Override
+        public void updateItem(Session session, boolean empty) {
+            super.updateItem(session, empty);
+        
+            if (empty) {
+                setText(null);
+                setGraphic(null);
+            } else {
+                if (sessionHBox == null) {
+                    sessionHBox = new SessionHBox(session);
+                } 
+                setGraphic(sessionHBox);
+            }
+        }
+    }
+
+    public class SessionHBox extends HBox {
+
+        Label nameLabel;
+        ImageView imageView;
+        Button continuePartyButton;
+
+        public SessionHBox(Session session) {
+            
+            imageView = new ImageView();
+            imageView.setImage(new Image(session.getTamagotchi_img_path()));
+            imageView.setFitWidth(100); // Set maximum width to 100 pixels
+            imageView.setFitHeight(100);
+
+            nameLabel = new Label(session.getNom_donner_tamagotchi());
+            nameLabel.setStyle("-fx-font-size: 14px; -fx-font-family: Arial;"); // Change font size and family
+            
+            continuePartyButton = new Button("Continue Party");
+
+            this.getChildren().addAll(imageView, nameLabel,continuePartyButton);
+            this.setSpacing(10);
+            this.setAlignment(Pos.CENTER_LEFT);
+        }
+
+    }
 
     ArrayList<Session> allSessions;
+
+    @FXML
+    AnchorPane rootLayout;
+
+    
+    ListView<Session> sessionListView;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+       
         allSessions = JsonDatabase.getAllSession();
-        // Assuming `allSessions` is populated with data
+            // Set up the ListView to display sessions
+        sessionListView = new ListView<>();
+        
+        sessionListView.setCellFactory(listView -> new SessionListViewCell());
+
         for (Session session : allSessions) {
-            TableColumn<Session, String> tableColumn = new TableColumn<>(session.getNom_donner_tamagotchi());
-
-  
-
-            tableColumn.setCellValueFactory(data -> {
-                return new SimpleStringProperty("MON TAMA");
-            });
-            
-
-            tableView.getColumns().add(tableColumn);
-            tableView.getItems().add(session);
+            // Create a custom cell to display the session image and name
+            sessionListView.getItems().add(session);
         }
-        //tableView.getItems().addAll(allSessions);
-    }
-    
-}
 
+        sessionListView.prefHeightProperty().bind(rootLayout.prefHeightProperty());
+        sessionListView.prefWidthProperty().bind(rootLayout.prefWidthProperty());
+        rootLayout.getChildren().add(sessionListView);
+       
+    }    
+}
