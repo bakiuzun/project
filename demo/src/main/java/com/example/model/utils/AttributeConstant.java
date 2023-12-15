91dd6b7f82ed0dc1fa623d2be195a9fe4df4cf61
matteopaquiry <145860073+matteopaquiry@users.noreply.github.com>
2023-12-15 11:08:59 +0100

    correction voiture,robot et ajout attribut constant

--- demo/src/main/java/com/example/model/utils/AttributeConstant.java
+++ demo/src/main/java/com/example/model/utils/AttributeConstant.java
@@ -32,4 +32,34 @@ public class AttributeConstant {
     public final static String TEMPERATURE = "Température";
     public final static String RUST = "Rouille";
     public final static String TAMAGOTCHI_IMG_PATH = "tamagotchi_img_path";
+
+    //Pas terminé
+    public final static String ACTION_EATING_CAT = "Donner de la paté";
+    public final static String ACTION_WASHING_CAT = "Brosser";
+    public final static String ACTION_PLAYING_CAT = "Donner une pelotte de laine";
+    public final static String ACTION_SLEEPING_CAT = "Dormir dans son perchoir";
+    public final static String ACTION_DOING_SPORT_CAT = "Utiliser le tapis d'agilité";
+    public final static String ACTION_USING_TOILET_CAT = "Utiliser la litière";
+
+    public final static String ACTION_EATING_DOG = "Donner de la viande";
+    public final static String ACTION_WASHING_DOG = "Laver le chier";
+    public final static String ACTION_PLAYING_DOG = "Aboyer";
+    public final static String ACTION_SLEEPING_DOG = "Dormir dans la niche";
+    public final static String ACTION_DOING_SPORT_DOG = "Envoyer un morceau de bois";
+    public final static String ACTION_USING_TOILET_DOG = "Emmener ailleurs";
+
+    public final static String ACTION_EATING_RABBIT = "Donner une carrotte";
+    public final static String ACTION_WASHING_RABBIT = "Brosser";
+    public final static String ACTION_PLAYING_RABBIT = "Donner un jeux d'intelligence";
+    public final static String ACTION_SLEEPING_RABBIT = "Dormir dans son terrier";
+    public final static String ACTION_DOING_SPORT_RABBIT = "Roue d'exercice";
+    public final static String ACTION_USING_TOILET_RABBIT = "Utiliser le tapis de propreté";
+
+    public final static String ACTION_EATING_TURTLE = "Donner de la salade";
+    public final static String ACTION_WASHING_TURTLE = "Brosser";
+    public final static String ACTION_PLAYING_TURTLE = "Carresser sa carapace";
+    public final static String ACTION_SLEEPING_TURTLE = "Dormir";
+    public final static String ACTION_DOING_SPORT_TURTLE = "Le dire de venir";
+    public final static String ACTION_USING_TOILET_TURTLE = "Faire ses besoins dans le coin";
+
 }
