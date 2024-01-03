7e2aa502c91adbd69a0f5fcf91183174eceeab0c
bakiuzun <u.officialdeveloper@gmail.com>
2024-01-03 19:01:46 +0100

    tiny change

--- demo/src/main/java/com/example/Helper.java
+++
@@ -1,8 +0,0 @@
-/*
-POP UP ALERT
-Alert alert = new Alert(AlertType.WARNING);
-alert.setTitle("Warning");
-alert.setHeaderText("Value is less than 0");
-alert.setContentText("The tracked value is less than 0: IT IS ALL OVER" );
-alert.showAndWait();
-*/
